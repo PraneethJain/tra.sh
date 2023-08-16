fb5321073e53dec10f281a29592c078b08fd11d9
PraneethJain <praneethjain005@gmail.com>
2023-08-16 14:53:01 +0530

    Added username and hostname

--- prompt.c
+++ prompt.c
@@ -1,6 +1,17 @@
 #include "headers.h"
 
-void prompt() {
-    // Do not hardcode the prmopt
-    printf("<Everything is a file> ");    
+void prompt()
+{
+  string username = new_string(256);
+  if (getlogin_r(username.str, username.size) != 0)
+  {
+    // Do error handling
+  }
+
+  string hostname = new_string(256);
+  if (gethostname(hostname.str, hostname.size) != 0)
+  {
+    // Do error handling
+  }
+  printf("<%s@%s> ", username.str, hostname.str);
 }
