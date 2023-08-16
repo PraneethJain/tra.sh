fa03edc8455356269af8d114377739cca2177ccf
PraneethJain <praneethjain005@gmail.com>
2023-08-16 15:30:17 +0530

    Only replacing when needed

--- prompt.c
+++ prompt.c
@@ -23,9 +23,12 @@ void prompt()
   // Replace /home/<username> with ~
   string homepath = new_string(512);
   sprintf(homepath.str, "/home/%s", username.str);
-  string tilde = new_string(2);
-  tilde.str = "~\0";
-  replace(&cwd, homepath, tilde);
+  if (strstr(cwd.str, homepath.str) == cwd.str) // If cwd starts with homepath
+  {
+    string tilde = new_string(2);
+    tilde.str = "~\0";
+    replace(&cwd, homepath, tilde);
+  }
 
   printf("<%s@%s:%s> ", username.str, hostname.str, cwd.str);
 }
