0a2c5d18280114257caa2335ec4c4b6100217e1c
PraneethJain <praneethjain005@gmail.com>
2023-08-18 10:55:03 +0530

    warp output

--- warp.c
+++ warp.c
@@ -2,9 +2,16 @@
 
 void warp(command c)
 {
+  string cwd = new_string(PATH_MAX);
+
   if (c.argc == 1)
   {
     chdir(homepath.str);
+    if (getcwd(cwd.str, cwd.size) == NULL)
+    {
+      // Do error handling
+    }
+    printf("%s\n", cwd.str);
   }
   else
   {
@@ -17,30 +24,13 @@ void warp(command c)
       else if (chdir(c.argv[i]) != 0)
       {
         printf("Cannot warp to %s\n", c.argv[i]);
+        break;
       }
+      if (getcwd(cwd.str, cwd.size) == NULL)
+      {
+        // Do error handling
+      }
+      printf("%s\n", cwd.str);
     }
   }
-
-  // string tok = new_string(4096);
-  // string delimiters = new_string(128);
-  // strcpy(delimiters.str, " \t\n\v\f\r");
-  //
-  // bool has_argument = false;
-  // while ((tok.str = strtok(NULL, delimiters.str)) != NULL)
-  // {
-  //   has_argument = true;
-  //   if (strcmp(tok.str, "~") == 0)
-  //   {
-  //     chdir(homepath.str);
-  //   }
-  //   else if (chdir(tok.str) != 0)
-  //   {
-  //     printf("Cannot warp to %s\n", tok.str);
-  //   }
-  // }
-  //
-  // if (!has_argument)
-  //   chdir(homepath.str);
-  //
-  // printf("\n");
 }
