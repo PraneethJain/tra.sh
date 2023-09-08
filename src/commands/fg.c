e7b13f7e02e136c234ed740f1f83fd9db00d3581
PraneethJain <praneethjain005@gmail.com>
2023-09-08 15:47:10 +0530

    implemented fg

--- src/commands/fg.c
+++ src/commands/fg.c
@@ -2,6 +2,28 @@
 
 int fg(command c)
 {
-  c.argc += 1;
+  if (c.argc != 2)
+  {
+    ERROR_PRINT("fg expected 1 argument, found %i\n", c.argc - 1);
+    return FAILURE;
+  }
+
+  if (!is_numeric(c.argv[1]))
+  {
+    ERROR_PRINT("fg found non integer PID %s\n", c.argv[1]);
+    return FAILURE;
+  }
+
+  char *temp;
+  pid_t pid = strtoll(c.argv[1], &temp, 10);
+  if (getpgid(pid) < 0) // Checks if process id is invalid
+  {
+    ERROR_PRINT("No process with pid %i found\n", pid);
+    return FAILURE;
+  }
+
+  int status;
+  waitpid(pid, &status, 0);
+
   return SUCCESS;
 }
